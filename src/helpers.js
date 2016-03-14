9689466963c8bd32db54625166f1f94ee0e0f593
DaveJ <dave@davejeffery.com>
2016-03-14 17:03:00 +0000

    Use gaeron's shallowEqual algo instead

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,26 @@
-import shallowEqual from 'is-equal-shallow';
 import { bindActionCreators } from 'redux';
 
-export const observeStore = (store, currState, select, onChange) => {
+export function shallowEqual(objA, objB) {
+  if (objA === objB) return true;
+
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  if (keysA.length !== keysB.length) return false;
+
+  // Test for A's keys different from B.
+  const hasOwn = Object.prototype.hasOwnProperty;
+  for (let i = 0; i < keysA.length; i++) {
+    if (!hasOwn.call(objB, keysA[i]) ||
+        objA[keysA[i]] !== objB[keysA[i]]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+export function observeStore(store, currState, select, onChange) {
   if (typeof onChange !== 'function') return null;
   let currentState = currState || {};
 
@@ -17,7 +36,7 @@ export const observeStore = (store, currState, select, onChange) => {
   const unsubscribe = store.subscribe(handleChange);
   handleChange();
   return unsubscribe;
-};
+}
 
 export const wrapActionCreators = (actionCreators) =>
   dispatch => bindActionCreators(actionCreators, dispatch);
