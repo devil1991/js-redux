8949ad88decb878db4ef3784c4579d25282454cb
DaveJ <dave@davejeffery.com>
2017-03-03 22:51:26 +0000

    lint fix

--- test/utils/redux.js
+++ test/utils/redux.js
@@ -1,3 +1,3 @@
-export const storeReducer = (x = [], y) => y.type !== '@@redux/INIT' ? [...x, y] : x;
+export const storeReducer = (x = [], y) => (y.type !== '@@redux/INIT' ? [...x, y] : x);
 export const connectState = state => state;
 export const connectActions = { foo: () => ({ type: 'bar' }) };
